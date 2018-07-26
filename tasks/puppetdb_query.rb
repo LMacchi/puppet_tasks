71450afd4aef855d7cc0f5e2800b1f388b97d69c
Laura Macchi <lau.macchi@gmail.com>
2018-07-26 14:10:26 -0500

    Fix argument passing

--- tasks/puppetdb_query.rb
+++ tasks/puppetdb_query.rb
@@ -8,7 +8,7 @@ rescue LoadError
   raise 'PuppetDB utilities not found.  Is this the PuppetDB server?'
 end
 
-query = PT_query
+query = ENV['PT_query']
 
 def puppetdb_query(query)
   Puppet::Util::Puppetdb.query_puppetdb(query)
