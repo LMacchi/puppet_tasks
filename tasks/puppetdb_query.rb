5d9427bf5e89c06cc35d87fc769f699070aeb694
Laura Macchi <lau.macchi@gmail.com>
2018-07-26 13:35:39 -0500

    Add pdb task

---
+++ tasks/puppetdb_query.rb
@@ -0,0 +1,23 @@
+#!/opt/puppetlabs/puppet/bin/ruby
+
+require 'puppet'
+Puppet.initialize_settings
+begin
+  require 'puppet/util/puppetdb'
+rescue LoadError
+  raise 'PuppetDB utilities not found.  Is this the PuppetDB server?'
+end
+
+query = PT_query
+
+def puppetdb_query(query)
+  Puppet::Util::Puppetdb.query_puppetdb(query)
+end
+
+begin
+  puts Puppet::Util::Puppetdb.query_puppetdb(query).to_json
+  exit 0
+rescue Puppet::Error => e
+  puts({ status: 'failure', error: e.message }.to_json)
+  exit 1
+end
